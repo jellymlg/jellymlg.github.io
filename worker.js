d34002320f3adfe0eeb6d11bc410d3da203bcf5c
jellymlg <39315532+jellymlg@users.noreply.github.com>
2020-08-28 23:35:43 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,17 @@
 var age;
 var back;
-while(true) {
-  age = new Date(Date.now() - new Date("Wen, 28 Aug 2003 2:00:00 GMT").getTime() - 62167223780000);
+function get() {
+	age = new Date(Date.now() - new Date("Wen, 28 Aug 2003 2:00:00 GMT").getTime() - 62167223780000);
 	back = {
 		year: age.getFullYear(),
 		month: age.getMonth(),
 		day: (age.getDate() - 1),
 		hour: age.getHours(),
 		minute: age.getMinutes(),
-		seconds: age.getSeconds(),
+		second: age.getSeconds(),
 		millisecond: age.getMilliseconds()
 	};
 	postMessage(JSON.stringify(back));
+	setTimeout("get()", 100);
 }
+get();
